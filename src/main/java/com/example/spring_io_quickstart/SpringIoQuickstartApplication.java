15d758fd54002c2bea8395e7d3234c8e887f8220
NikosX <nikosxanos@gmail.com>
2020-04-16 01:23:09 +0300

    initial spring_io_quickstart commit

---
+++ src/main/java/com/example/spring_io_quickstart/SpringIoQuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_io_quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIoQuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIoQuickstartApplication.class, args);
+    }
+
+}
